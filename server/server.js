15b5c4557a7e8602c1ca75078f59ab59b3c370e1
Sophie Sherman <soph.sherman@hotmail.com>
2020-09-25 09:41:12 +1000

    Adding Procfile

--- server/server.js
+++ server/server.js
@@ -111,7 +111,7 @@ app.post('/api/login', async (request, response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
