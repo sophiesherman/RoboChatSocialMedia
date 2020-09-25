2cba20e4a0522e0ce13005ee981e122d80a4e362
Sophie Sherman <soph.sherman@hotmail.com>
2020-09-25 11:40:02 +1000

    Adding api.js

---
+++ server/controllers/api.js
@@ -0,0 +1,126 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const fs = require('fs')
+
+const SECRET = process.env.SECRET
+
+// Load data from JSON file into memory
+const rawData = fs.readFileSync('server/sample.json')
+const data = JSON.parse(rawData)
+
+let posts = data.posts
+let users = data.users
+
+const getUser = (username) => {
+  return data.users.filter(u => u.id === username)[0]
+}
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization') 
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) { 
+         return authorization.substring(7)  
+      }  
+  return null
+} 
+
+const apiRouter = express.Router()
+
+apiRouter.get('/', (request, response) => {
+  response.send('<h1>Web Server</h1>')
+})
+
+apiRouter.get('/api/posts', (request, response) => {
+  console.log("GOT")
+  response.json(posts)
+})
+
+apiRouter.get('/api/users', (request, response) => {
+    console.log("GOT")
+    response.json(users)
+  })
+
+apiRouter.get('/api/posts/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const post = posts.find(post => post.id === id)
+	if (post) response.json(post)
+	else response.status(404).end()
+})
+
+apiRouter.get('/api/users/:id', (request, response) => {
+	const id = request.params.id
+	const user = users.find(user => user.id === id)
+	if (user) response.json(user)
+	else response.status(404).end()
+})
+
+apiRouter.post('/api/posts', (request, response) => {
+  console.log("POST")
+  const post = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  posts = posts.concat(post)
+  response.json(post)
+})
+
+apiRouter.post('/api/users', (request, response) => {
+    console.log("POST")
+    const user = request.body
+    users = users.concat(user)
+    response.json(user)
+  })
+
+apiRouter.put('/api/posts/:id', (request, response) => {
+    const id = Number(request.params.id) 
+    let post = posts.find(post => post.id === id)
+    post = post.likes.concat("User")
+    if (post) response.json(post)
+    else response.status(404).end()
+})
+
+apiRouter.put('/api/users/:id', (request, response) => {
+    const id = request.params.id
+    let user = users.find(user => user.id === id)
+    user = user.follows.concat(id)
+    if (user) response.json(user)
+    else response.status(404).end()
+})
+
+apiRouter.delete('/api/posts/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log('the passed ID is: ', id)
+    posts = posts.filter(post => post.id !== id)
+    response.status(404).end()
+})
+
+// handle post request for login with {username, password}
+apiRouter.post('/api/login', async (request, response) => {
+    const {username, password} = request.body
+
+    const user = getUser(username)
+    
+    if (!user) {
+      return response.status(401).json({error: "Invalid username or password"})
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+      console.log("Password is good!")
+
+      const userForToken = {
+        id: user.id
+      }
+
+      const token = jwt.sign(userForToken, process.env.SECRET)
+
+      return response.status(200).json({token, id: user.id})
+
+    } else {
+      return response.status(401).json({error: "Invalid username or password"})
+    }
+})
+
+module.exports = apiRouter
--- server/server.js
+++ server/server.js
@@ -1,132 +1,14 @@
 require('dotenv').config()
+
 const express = require('express')
-const fs = require('fs')
-const app = express()
 const cors = require('cors')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const apiRouter = require("./controllers/api")
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
-
-const SECRET = process.env.SECRET
-
-// Load data from JSON file into memory
-const rawData = fs.readFileSync('server/sample.json')
-const data = JSON.parse(rawData)
-
-let posts = data.posts
-let users = data.users
-
-const getUser = (username) => {
-  return data.users.filter(u => u.id === username)[0]
-}
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization') 
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) { 
-         return authorization.substring(7)  
-      }  
-  return null
-} 
-
-app.get('/', (request, response) => {
-  response.send('<h1>Web Server</h1>')
-})
-
-app.get('/api/posts', (request, response) => {
-  console.log("GOT")
-  response.json(posts)
-})
-
-app.get('/api/users', (request, response) => {
-    console.log("GOT")
-    response.json(users)
-  })
-
-app.get('/api/posts/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const post = posts.find(post => post.id === id)
-	if (post) response.json(post)
-	else response.status(404).end()
-})
-
-app.get('/api/users/:id', (request, response) => {
-	const id = request.params.id
-	const user = users.find(user => user.id === id)
-	if (user) response.json(user)
-	else response.status(404).end()
-})
-
-app.post('/api/posts', (request, response) => {
-  console.log("POST")
-  const post = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, SECRET)
-  if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-
-  posts = posts.concat(post)
-  response.json(post)
-})
-
-app.post('/api/users', (request, response) => {
-    console.log("POST")
-    const user = request.body
-    users = users.concat(user)
-    response.json(user)
-  })
-
-app.put('/api/posts/:id', (request, response) => {
-    const id = Number(request.params.id) 
-    let post = posts.find(post => post.id === id)
-    post = post.likes.concat("Test")
-    if (post) response.json(post)
-    else response.status(404).end()
-})
-
-app.put('/api/users/:id', (request, response) => {
-    const id = request.params.id
-    let user = users.find(user => user.id === id)
-    user = user.follows.concat(id)
-    if (user) response.json(user)
-    else response.status(404).end()
-})
-
-app.delete('/api/posts/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log('the passed ID is: ', id)
-    posts = posts.filter(post => post.id !== id)
-    response.status(404).end()
-})
-
-// handle post request for login with {username, password}
-app.post('/api/login', async (request, response) => {
-    const {username, password} = request.body
-
-    const user = getUser(username)
-    
-    if (!user) {
-      return response.status(401).json({error: "Invalid username or password"})
-    }
-
-    if (await bcrypt.compare(password, user.password)) {
-      console.log("Password is good!")
-
-      const userForToken = {
-        id: user.id
-      }
-
-      const token = jwt.sign(userForToken, process.env.SECRET)
-
-      return response.status(200).json({token, id: user.id})
-
-    } else {
-      return response.status(401).json({error: "Invalid username or password"})
-    }
-})
+app.use(apiRouter)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
